9699452e08f9bf022d42dc2b714a27c393513dec
Bryan Williamson <williamson.bryanm@gmail.com>
2024-10-16 18:35:39 -0700

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Bryan Williamson on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
